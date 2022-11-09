d73be703609f31205e433022cf0af3087edffc3e
HusseinHany <hhany3997@gmail.com>
2022-11-09 19:57:51 +0200

    Solve A - 1 to N

---
+++ Level 1 Training/Week 2 -Loops-/A - 1 to N.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+ * as simple as iterating from 1 to N and printing your current iteration number
+ */
+
+int main() {
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
