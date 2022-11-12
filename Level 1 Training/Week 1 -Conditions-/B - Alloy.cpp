5c5c5eec0c428b6c79b257fea72c041aad51a1db
Hussein Hany <hh1681@fayoum.edu.eg>
2022-11-12 17:59:57 +0200

    Update B - Alloy.cpp

--- Level 1 Training/Week 1 -Conditions-/B - Alloy.cpp
+++ Level 1 Training/Week 1 -Conditions-/B - Alloy.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
-
-
 using namespace std;
 
 int main() {
-    int a, b   ;
+    int a, b;
     cin >> a >>  b ;
 
-    if ( a >  0 && b > 0)
-        cout<< "Alloy" ;
+    if (a > 0 && b > 0)
+        cout<< "Alloy";
     else if (b > 0)
-        cout << "Silver" ;
+        cout << "Silver";
     else
-        cout << "Gold" ;
-
+        cout << "Gold";
 }
