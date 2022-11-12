2a5b1202de17fc9464606a36673c84d79b516949
Hussein Hany <hh1681@fayoum.edu.eg>
2022-11-12 18:16:40 +0200

    Update M - Buttons.cpp

--- Level 1 Training/Week 1 -Conditions-/M - Buttons.cpp
+++ Level 1 Training/Week 1 -Conditions-/M - Buttons.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 /*
- * simple solutaion is to check
- *     if a == b than press a and b once
+ * simple solution is to check
+ *     if a == b then press a and b once ..
  *     else press max(a,b) twice
  *
  */
@@ -13,11 +13,14 @@ int main(){
 
     int a,b;
     cin>>a>>b;
+    
     // simple trick make (a) always max
     if (b > a)
         swap(a,b);
+    
     if (a == b)
         cout << 2 * a;
-    else cout << a + (a-1);
+    else 
+        cout << a + (a-1);
 
 }
