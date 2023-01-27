f35ba903aa295b86becc203d3855f9d4c1913298
HusseinHany <hhany3997@gmail.com>
2023-01-27 13:21:43 +0200

    Solve B

---
+++ Level 1 Training/We pretend it's an ECPC | Upsolving/B - Imminent.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define endl        "\n"
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+/*
+ * Just loop 4 times each cin number of push ups this week ..
+ * if thus number is >= 10 then count it with u
+ */
+
+int main() {
+    int cnt = 0;
+
+    for (int i = 0; i < 4; ++i) {
+        int pushups;
+        cin >> pushups;
+
+        if (pushups >= 10) cnt++;
+    }
+
+    cout << cnt;
+}
