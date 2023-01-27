b3eb59d5cf5402d6c4284ba6aeff01fe70be18b0
HusseinHany <hhany3997@gmail.com>
2023-01-27 13:32:56 +0200

    Solve D

---
+++ Level 1 Training/We pretend it's an ECPC | Upsolving/D - Duplicated version but.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define endl        "\n"
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+/*
+ * The answer is in this sentence
+ * Hussein categorises a Facebook account as spam
+ * if, the following count of the account is more than 10 times the count of followers.
+ *
+ * you're given the following count as X ..
+ * and the followers count as Y ..
+ *
+ * Just check if x is greater than y * 10
+ */
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+
+        if (x > (y * 10))
+            cout << "yes\n";
+        else
+            cout << "no\n";
+    }
+}
