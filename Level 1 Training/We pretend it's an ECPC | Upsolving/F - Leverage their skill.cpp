5fc3e74468528036961b42575227425efe2f6ab4
HusseinHany <hhany3997@gmail.com>
2023-01-27 13:47:54 +0200

    Solve F

---
+++ Level 1 Training/We pretend it's an ECPC | Upsolving/F - Leverage their skill.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define pb          push_back
+#define yes         cout<<"YES"<<endl
+#define no          cout<<"NO"<<endl
+#define endl        "\n"
+#define improve     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      My way to My dream....
+*/
+
+/*
+ * To say that a divides b .. so b % a == 0
+ * therefor .. a must be less than b ..
+ *
+ * so the only possible case to find a number that divides all the array
+ * is that it is the minimum among them ..
+ *
+ * you can simply sort the array and iterate over all elements
+ * if arr[i] % arr[0] == 0
+ * means if current element is divisible by the smallest element that we are currently testing
+ * count it
+ *
+ * afterwards .. if that count is == n then all elements pf the array are divisible by arr[0]
+ * otherwise it is impossible ti find such a number, and we print -1
+ */
+
+int main() {
+    ll n;
+    cin >> n;
+
+    ll arr[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+    ll cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] % arr[0] == 0)
+            cnt++;
+    }
+
+    if (cnt == n)
+        cout << cnt;
+    else
+        cout << -1;
+}
