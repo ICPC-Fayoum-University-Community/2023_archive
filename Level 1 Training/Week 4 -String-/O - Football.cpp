8b612fe23f6802b88fca00b0434dee87e4c97ae2
HusseinHany <hhany3997@gmail.com>
2023-02-03 22:34:51 +0200

    Solve O - Football

---
+++ Level 1 Training/Week 4 -String-/O - Football.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define pb          push_back
+#define yes         cout<<"yes"<<endl
+#define no          cout<<"no"<<endl
+#define endl        "\n"
+#define improve     ios_base::sync_with_stdio(false); cin.tie(null); cout.tie(null)
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      my way to my dream....
+*/
+
+/*
+ * Just check if the substrings "1111111" or "0000000" exist on the given input
+ */
+
+int main() {
+    string s;
+    cin >> s;
+
+    if (s.find("1111111") != string::npos || s.find("0000000") != string::npos)
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
