50cca018e4a50b2bb5fa750776c071668022b84c
HusseinHany <hhany3997@gmail.com>
2023-02-03 21:57:47 +0200

    Solve L - Stones on the Table

---
+++ Level 1 Training/Week 4 -String-/L - Stones on the Table.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define pb          push_back
+#define yes         cout<<"yes"<<endl
+#define no          cout<<"no"<<endl
+#define endl        "\n"
+#define improve     ios_base::sync_with_stdio(false); cin.tie(null); cout.tie(null)
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      my way to my dream....
+*/
+
+/*
+ * Just count how many stones to be removed ..
+ * A stone should be removed if it is the same is its next one.
+ */
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int cnt = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        if (s[i] == s[i + 1])
+            ++cnt;
+    }
+
+    cout << cnt;
+    return 0;
+}
