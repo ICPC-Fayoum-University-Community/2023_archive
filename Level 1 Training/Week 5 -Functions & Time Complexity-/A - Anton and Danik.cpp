d114cd374fdfb123b33445df423713b8afc316f4
HusseinHany <hhany3997@gmail.com>
2023-02-08 07:21:12 +0200

    Solve A - Anton and Danik

---
+++ Level 1 Training/Week 5 -Functions & Time Complexity-/A - Anton and Danik.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define lp1(n)      for(ll i = 1; i <= (n); ++i)
+#define lp(n)       for(ll i = 0; i < (n); ++i)
+#define tc          ll testcase;   cin>>testcase;   while(testcase--)
+#define all(v)      (v).begin(), (v).end()
+#define allr(v)     (v).rbegin(), (v).rend()
+#define pb          push_back
+#define yes         cout<<"yes"<<endl
+#define no          cout<<"no"<<endl
+#define endl        "\n"
+#define improve     ios_base::sync_with_stdio(false); cin.tie(null); cout.tie(null)
+int dx[4]{-1, 0, 1, 0};
+int dy[4]{0, 1, 0, -1};
+
+/* 3ein39
+                    " وَأَن لَّيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَى ﴿39﴾ وَأَنَّ سَعْيَهُ سَوْفَ يُرَى ﴿40﴾ ثُمَّ يُجْزَاهُ الْجَزَاء الْأَوْفَى "
+                                      my way to my dream....
+*/
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    int a = 0, d = 0;
+    for (int i = 0; i < s.size(); ++i) {
+        s[i] == 'A' ? ++a : ++d;
+    }
+
+    if (a > d)
+        cout << "Anton";
+    else if (d > a)
+        cout << "Danik";
+    else
+        cout << "Friendship";
+
+    return 0;
+}
