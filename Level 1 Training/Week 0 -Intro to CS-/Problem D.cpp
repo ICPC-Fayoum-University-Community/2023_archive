dcc03b4e51971c8d04a6808b0026c43ebac059d2
Hussein Hany <hh1681@fayoum.edu.eg>
2023-03-29 01:33:50 +0200

    Create Problem D.cpp

---
+++ Level 1 Training/Week 0 -Intro to CS-/Problem D.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#define Ziad_Ahmed ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0);
+#define ll long long 
+#define el '\n'
+using namespace std;
+
+int main()
+{
+    Ziad_Ahmed;
+    
+    int n, w; 
+    cin >> n >> w; 
+    cout << n / w << endl;
+    return 0;
+}
