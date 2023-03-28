093f559a7f2facc59a66d4531ff595e88d2f3347
BluoCaroot <114243953+BluoCaroot@users.noreply.github.com>
2023-03-28 14:21:01 +0200

    Update Problem I.cpp
    Added coments

--- Level 1 Training/Week 0 -Intro to CS-/Problem I.cpp
+++ Level 1 Training/Week 0 -Intro to CS-/Problem I.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-	int x;
+	int x; // our number is in the range of integer so no need to use long long
 	cin >> x;
-	cout << (x + 4) / 5;
+	cout << (x + 4) / 5; //we approximate our number x to the nearest greatest factor of 5 then we divide it by 5
 }
