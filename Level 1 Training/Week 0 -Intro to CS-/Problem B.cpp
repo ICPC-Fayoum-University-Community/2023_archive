650a74b97effde3c4d3db4e623290e214983a0ee
Hussein Hany <hh1681@fayoum.edu.eg>
2023-03-29 21:16:41 +0200

    Create Problem B.cpp

---
+++ Level 1 Training/Week 0 -Intro to CS-/Problem B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+    
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl
+        << b << endl
+        << c << endl
+        << d << endl
+        << e << endl;
+
+    return 0;
+}
