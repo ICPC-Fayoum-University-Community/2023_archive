5f86267ce16cc6fa60bf73434434f3c31d37e725
BluoCaroot <assem.akl2004@gmail.com>
2023-09-11 23:04:11 +0300

    contest #2 upsolve

---
+++ Level 1 Training/Wave #2 Contests/Contest #2/Problem E.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+	int t; cin >> t; while (t--)
+	{
+		int x, y;
+
+		cin >> x >> y;
+		cout << 2 << '\n';
+		cout << x - 1 << ' ' << 1 << '\n';
+		cout << x << ' ' << y << '\n';
+	}
+}
